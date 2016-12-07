f83ddc144cfb7eb2d9ce8ec1458f834c0d598db9
Gowthami Volete <gowthami30sunil@yahoo.com>
2016-12-07 01:39:23 -0500

    creating repository

---
+++ src/com/shu/android/rest/poc/Constants.java
@@ -0,0 +1,9 @@
+package com.shu.android.rest.poc;
+
+public class Constants {
+	public static String dbClass = "com.mysql.jdbc.Driver";
+    private static String dbName= "androidpoc";
+    public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
+    public static String dbUser = "root";
+    public static String dbPwd = "mysql";
+}
